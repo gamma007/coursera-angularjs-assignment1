eba3e0c293b840c7fa197178ddff93720c9f07fd
mayur <gamma007@gmail.com>
2017-10-03 22:23:59 +0530

    Added logic for undefined inputText.

--- app.js
+++ app.js
@@ -12,15 +12,24 @@
 			var message = $scope.inputText;
 			$scope.messageColor = "green";
 
-			if(message.length == 0) {
+			if(message == undefined || message.length == 0) {
 				$scope.message = "Please enter data first";
 				$scope.messageColor = "red";
 			} else {
 				var msg = message.split(',');
-				if(msg.length <= 3) {
+				var count = 0;
+				for(var i = 0; i < msg.length; i++) {
+					if(msg[i].length == 0) {
+						continue;
+					}
+					count++;
+				}
+				if(count <= 3) {
 					$scope.message = "Enjoy!";
+					$scope.messageColor = "green";
 				} else {
 					$scope.message = "Too much!";
+					$scope.messageColor = "green";
 				}
 			}
 		}
